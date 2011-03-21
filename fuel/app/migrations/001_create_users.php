c070b840da814b3706d9044c69f5b10a16ff1aeb
crynobone <crynobone@gmail.com>
2011-03-21 23:09:23 +0800

    fix field name typo

--- fuel/app/migrations/001_create_users.php
+++ fuel/app/migrations/001_create_users.php
@@ -10,7 +10,7 @@ class Create_users {
 			'id' => array('constraint' => 30, 'type' => 'bigint', 'auto_increment' => true),
 			'user_name' => array('constraint' => 50, 'type' => 'varchar'),
 			'email' => array('constraint' => 100, 'type' => 'varchar'),
-			'status' => array('constraint' => "'unverifed','verified','banned'", 'type' => 'enum', 'default' => 'unverified'),
+			'status' => array('constraint' => "'unverified','verified','banned'", 'type' => 'enum', 'default' => 'unverified'),
 
         ), array('id'));
     }
