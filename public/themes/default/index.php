28f1d688b2b2e508123496bb10714323b1c216dd
crynobone <crynobone@gmail.com>
2011-03-24 23:37:41 +0800

    Html::title already return <title>, don't wrap it again.

--- public/themes/default/index.php
+++ public/themes/default/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo \Hybrid\Html::title($title); ?></title>
+	<?php echo \Hybrid\Html::title($title); ?>
 	<style type="text/css">
 		* { margin: 0; padding: 0; }
 		body { background-color: #EEE; font-family: sans-serif; font-size: 16px; line-height: 20px; margin: 40px; }
