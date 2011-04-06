3df62644af79da566762eac22e4940b8bf1146aa
crynobone <crynobone@gmail.com>
2011-04-07 01:35:07 +0800

    partial

--- fuel/app/classes/controller/api/credential.php
+++ fuel/app/classes/controller/api/credential.php
@@ -1,18 +1,18 @@
 <?php
 
 class Controller_Api_Credential extends \Hybrid\Controller_Rest {
-	
+
 	public function post_login() {
 		$username = \Input::post('username');
 		$password = \Input::post('password');
-		
+
 		$result = \Hybrid\Acl_User::login($username, $password);
-		
+
 		if (false === $result) {
 			$this->response(array('text' => 'Invalid username and password combination'), 401);
-		}
-		else {
+		} else {
 			$this->response(array('text' => 'Login successful, redirecting now...', 'redirect' => \Uri::create('account/me')), 200);
 		}
 	}
+
 }
